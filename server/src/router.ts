a5a38d556c7361b7ef825a401d6a0c198129a663
Sophie LDL <sophievigneron33@gmail.com>
2026-09-01 11:12:45 +0200

    replaced schema.sql

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,11 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
-import itemActions from "./modules/item/itemActions.old";
+/* import itemActions from "./modules/item/itemActions.old";*/
 
+/*
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
+router.post("/api/items", itemActions.add); 
+*/
 
 /* ************************************************************************* */
 
