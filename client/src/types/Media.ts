6a40964535244f73d1c7849b8a6183adfd36c12c
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-09-10 09:46:14 +0200

    feat/US-REC-01 added type for Media, Person and SearchResults

---
+++ client/src/types/Media.ts
@@ -0,0 +1,22 @@
+type Media = {
+  id: number;
+  name: string;
+  poster: string | null;
+  releasedAt: string | null;
+};
+
+type Person = {
+  id: number;
+  name: string;
+  photo: string | null;
+};
+
+type SearchResults = {
+  films: Media[];
+  series: Media[];
+  animes: Media[];
+  actors: Person[];
+  hasMore: boolean;
+};
+
+export type { Media, Person, SearchResults };
