33884bf8a5e5a781a88d3898a33fc0e97c432af5
Sophie LDL <sophievigneron33@gmail.com>
2026-09-11 09:39:15 +0200

    fix: moved Catalog.tsx into DiscoverSection component

--- client/src/components/DiscoverSection.tsx
+++ client/src/components/DiscoverSection.tsx
@@ -1,8 +1,62 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import fetchDiscover from "../services/catalogService";
+import Carousel from "./Carousel";
+import MediaCard from "./MediaCard";
+import type { DiscoverResponse } from "../types/Catalog";
+
 function DiscoverSection() {
+  const [searchParams] = useSearchParams();
+  const type = searchParams.get("type");
+  const requestedType =
+    type === "movie"
+      ? type
+      : type === "tv"
+        ? type
+        : type === "anime"
+          ? type
+          : undefined;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [discover, setDiscover] = useState<DiscoverResponse>();
+  useEffect(() => {
+    fetchDiscover(requestedType)
+      .then((data) => {
+        setDiscover(data);
+      })
+      .catch((error) => console.log("Erreur", error));
+  }, [requestedType]);
   return (
     <>
-      <h1>hehe</h1>
-      <p>hihi</p>
+      {console.log(discover)}
+      <h2>Populaires</h2>
+      {discover ? (
+        <Carousel>
+          {discover?.topRated.map((media) => (
+            <MediaCard
+              key={media.id}
+              media={media}
+              className="catalog-carousel-item"
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <p>Chargement ...</p>
+      )}
+      <h2>Nouveautés</h2>
+      {discover ? (
+        <Carousel>
+          {discover?.topRated.map((media) => (
+            <MediaCard
+              key={media.id}
+              media={media}
+              className="catalog-carousel-item"
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <p>Chargement ...</p>
+      )}
     </>
   );
 }
--- client/src/pages/Catalog.tsx
+++ client/src/pages/Catalog.tsx
@@ -1,63 +1,10 @@
-import { useEffect, useState } from "react";
-import fetchDiscover from "../services/catalogService";
-import type { DiscoverResponse } from "../types/Catalog";
-import { useSearchParams } from "react-router";
-import Carousel from "../components/Carousel";
-import MediaCard from "../components/MediaCard";
+import DiscoverSection from "../components/DiscoverSection";
 
 function Catalog() {
-  const [searchParams] = useSearchParams();
-  const type = searchParams.get("type");
-  const requestedType =
-    type === "movie"
-      ? type
-      : type === "tv"
-        ? type
-        : type === "anime"
-          ? type
-          : undefined;
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [discover, setDiscover] = useState<DiscoverResponse>();
-  useEffect(() => {
-    fetchDiscover(requestedType)
-      .then((data) => {
-        setDiscover(data);
-      })
-      .catch((error) => console.log("Erreur", error));
-  }, [requestedType]);
   return (
     <>
       <h1>Catalogue</h1>
-      {console.log(discover)}
-      <h2>Populaires</h2>
-      {discover ? (
-        <Carousel>
-          {discover?.topRated.map((media) => (
-            <MediaCard
-              key={media.id}
-              media={media}
-              className="catalog-carousel-item"
-            />
-          ))}
-        </Carousel>
-      ) : (
-        <p>Chargement ...</p>
-      )}
-      <h2>Nouveautés</h2>
-      {discover ? (
-        <Carousel>
-          {discover?.topRated.map((media) => (
-            <MediaCard
-              key={media.id}
-              media={media}
-              className="catalog-carousel-item"
-            />
-          ))}
-        </Carousel>
-      ) : (
-        <p>Chargement ...</p>
-      )}
+      <DiscoverSection />
     </>
   );
 }
