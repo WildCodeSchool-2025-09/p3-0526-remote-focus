7c47b03b04e118eb1e4a2922c6e5600a3d1b173d
Sophie LDL <sophievigneron33@gmail.com>
2026-09-13 18:19:19 +0200

    feat: Added an error and loading functionnality

--- client/src/components/Catalog/DiscoverSection.tsx
+++ client/src/components/Catalog/DiscoverSection.tsx
@@ -3,10 +3,13 @@ import { useSearchParams } from "react-router";
 import fetchDiscover from "../../services/catalogService";
 import type { DiscoverResponse } from "../../types/Catalog";
 import MediaSection from "./MediaSection";
+import MediaCardLoading from "./MediaCardLoading";
 
 function DiscoverSection() {
   const [searchParams] = useSearchParams();
+
   const type = searchParams.get("type");
+
   const requestedType =
     type === "movie"
       ? type
@@ -15,52 +18,88 @@ function DiscoverSection() {
         : type === "anime"
           ? type
           : undefined;
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [discover, setDiscover] = useState<DiscoverResponse>();
+
   useEffect(() => {
+    setIsLoading(true);
+    setError(false);
+    setDiscover(undefined);
     fetchDiscover(requestedType)
       .then((data) => {
         setDiscover(data);
       })
-      .catch((error) => console.log("Erreur", error));
+      .catch((error) => {
+        console.error("Erreur lors du chargement du catalogue :", error);
+        setError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [requestedType]);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="skeleton h-8 w-48 mt-12 ml-4 mr-8" />
+        <div className="carousel flex">
+          {Array.from({ length: 10 }, (_, index) => index + 1).map(
+            (loadingId) => (
+              <MediaCardLoading key={loadingId} />
+            ),
+          )}
+        </div>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <p className="m-12">
+        Une erreur est survenue lors du chargement du catalogue. Merci
+        d'actualiser la page.
+      </p>
+    );
+  }
+
+  if (!discover) {
+    return null;
+  }
+
   return (
     <>
-      {discover ? (
-        <>
-          <MediaSection
-            title="Populaires"
-            medias={discover.topRated}
-            searchParams={requestedType}
-          />
-          <MediaSection
-            title="Nouveautés"
-            medias={discover.latest}
-            searchParams={requestedType}
-          />
-          <MediaSection
-            title={`${discover.genreSections[0].name}`}
-            medias={discover.genreSections[0].medias}
-            searchParams={requestedType}
-            genreId={discover.genreSections[0].id}
-          />
-          <MediaSection
-            title={`${discover.genreSections[1].name}`}
-            medias={discover.genreSections[1].medias}
-            searchParams={requestedType}
-            genreId={discover.genreSections[1].id}
-          />
-          <MediaSection
-            title={`${discover.genreSections[2].name}`}
-            medias={discover.genreSections[2].medias}
-            searchParams={requestedType}
-            genreId={discover.genreSections[2].id}
-          />
-        </>
-      ) : (
-        <p>Chargement ...</p>
-      )}
+      <>
+        <MediaSection
+          title="Populaires"
+          medias={discover.topRated}
+          searchParams={requestedType}
+        />
+        <MediaSection
+          title="Nouveautés"
+          medias={discover.latest}
+          searchParams={requestedType}
+        />
+        <MediaSection
+          title={`${discover.genreSections[0].name}`}
+          medias={discover.genreSections[0].medias}
+          searchParams={requestedType}
+          genreId={discover.genreSections[0].id}
+        />
+        <MediaSection
+          title={`${discover.genreSections[1].name}`}
+          medias={discover.genreSections[1].medias}
+          searchParams={requestedType}
+          genreId={discover.genreSections[1].id}
+        />
+        <MediaSection
+          title={`${discover.genreSections[2].name}`}
+          medias={discover.genreSections[2].medias}
+          searchParams={requestedType}
+          genreId={discover.genreSections[2].id}
+        />
+      </>
     </>
   );
 }
