d2d8a9cdb6f51a9cf01bfdc5b8ccf3ccdcfb9ae2
alexandravelisar <anda_alexandra91@yahoo.com>
2026-09-15 12:41:20 +0200

    feat(US-DET-02): add season episodes route

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import actorActions from "./modules/actor/actorActions";
 import mediaActions from "./modules/media/mediaActions";
+import seasonActions from "./modules/season/seasonActions";
 import serieActions from "./modules/serie/serieActions";
 
 const router = express.Router();
@@ -21,6 +22,7 @@ router.post("/api/items", itemActions.add);
 /* ************************************************************************* */
 router.get("/api/medias/:id", mediaActions.read);
 router.get("/api/series/:id", serieActions.read);
+router.get("/api/seasons/:id/episodes", seasonActions.readEpisodes);
 router.get("/api/actors/:id/filmography", actorActions.readFilmography);
 
 export default router;
