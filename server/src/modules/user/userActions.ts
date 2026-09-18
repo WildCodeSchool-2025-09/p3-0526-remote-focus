c2fa6bced06d209c23db3ac9fa5332e80b82d2ce
czagoh <dorian.pennegat@hotmail.fr>
2026-09-18 11:26:47 +0200

    feat: add user creation repository

---
+++ server/src/modules/user/userActions.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from "express";
+
+import type { RegisterUserInput } from "../../types/User/User.types";
+import UserRepository from "./userRepository";
+
+const add: RequestHandler = async (req, res, next) => {
+  try {
+    const newUser: RegisterUserInput = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName === "" ? null : req.body.lastName,
+      email: req.body.email,
+      bornAt: req.body.bornAt,
+      login: req.body.login,
+      password: req.body.password,
+    };
+
+    const insertId = await UserRepository.create(newUser);
+
+    res.status(201).json({
+      insertId,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { add };
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import actorActions from "./modules/actor/actorActions";
 import homepageActions from "./modules/homepage/homepageActions";
 import mediaActions from "./modules/media/mediaActions";
+import userActions from "./modules/user/userActions";
 
 const router = express.Router();
 
@@ -9,12 +10,9 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Define item-related routes
-/* import itemActions from "./modules/item/itemActions.old";*/
 import catalogActions from "./modules/catalog/catalogActions";
 
 router.get("/api/medias/discover", catalogActions.readDiscoverSections);
-
 router.get("/api/medias/home", homepageActions.browseHomepage);
 /*
 router.get("/api/items", itemActions.browse);
@@ -25,5 +23,7 @@ router.post("/api/items", itemActions.add);
 /* ************************************************************************* */
 router.get("/api/medias/:id", mediaActions.read);
 router.get("/api/actors/:id/filmography", actorActions.readFilmography);
+
+router.post("/api/users", userActions.add);
 
 export default router;
