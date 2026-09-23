126c32c64aed1455920c58b5e07422ce15a75410
Sophie LDL <sophievigneron33@gmail.com>
2026-09-23 17:27:43 +0200

    chore: removed a test line into actorActions.ts and api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -59,7 +59,7 @@ export async function fetchFilmography(
   page = 1,
 ): Promise<KnownForResponse> {
   const response = await fetch(
-    `${API_URL}/api/actors/${personId}/known-for?exclude=${excludeMediaId}&page=${page}&user=33`,
+    `${API_URL}/api/actors/${personId}/known-for?exclude=${excludeMediaId}&page=${page}`,
   );
 
   if (!response.ok) {
